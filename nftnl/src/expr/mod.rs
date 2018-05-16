1908001e361714d276f3d2783ddcc01b9627d889
Linus Färnstrand <linus@mullvad.net>
2018-05-17 00:17:36 +0200

    Add immediate and bitwise expressions

--- nftnl/src/expr/mod.rs
+++ nftnl/src/expr/mod.rs
@@ -6,11 +6,17 @@ pub trait Expression {
     fn to_expr(&self) -> Result<*mut sys::nftnl_expr>;
 }
 
+mod bitwise;
+pub use self::bitwise::*;
+
 mod cmp;
 pub use self::cmp::*;
 
 mod counter;
 pub use self::counter::*;
+
+mod immediate;
+pub use self::immediate::*;
 
 mod meta;
 pub use self::meta::*;
@@ -20,11 +26,20 @@ pub use self::payload::*;
 
 #[macro_export]
 macro_rules! nft_expr {
+    (bitwise mask $mask:expr,xor $xor:expr) => {
+        nft_expr_bitwise!(mask $mask, xor $xor)
+    };
     (cmp $op:tt $data:expr) => {
         nft_expr_cmp!($op $data)
     };
     (counter) => {
         $crate::expr::Counter
+    };
+    (verdict $verdict:ident) => {
+        nft_expr_verdict!($verdict)
+    };
+    (verdict $verdict:ident $chain:expr) => {
+        nft_expr_verdict!($verdict $chain)
     };
     (meta $expr:ident) => {
         nft_expr_meta!($expr)
