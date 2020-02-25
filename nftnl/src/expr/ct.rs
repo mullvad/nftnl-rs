744a4282d3fd6d3df7ba4351e6047c0e5598bb04
David Lönnhager <david.l@mullvad.net>
2020-02-25 15:14:25 +0100

    Add support for connmark

--- nftnl/src/expr/ct.rs
+++ nftnl/src/expr/ct.rs
@@ -13,23 +13,38 @@ bitflags::bitflags! {
 }
 
 pub enum Conntrack {
+    Mark,
     State,
 }
 
-impl Conntrack {
-    fn raw_key(&self) -> u32 {
-        match *self {
+pub struct ConntrackStruct {
+    ct_type: Conntrack,
+    set_value: bool,
+}
+
+impl ConntrackStruct {
+    pub fn new(ct_type: Conntrack, set_value: bool) -> Self {
+        ConntrackStruct { ct_type, set_value }
+    }
+
+    pub fn raw_key(&self) -> u32 {
+        match self.ct_type {
+            Conntrack::Mark => libc::NFT_CT_MARK as u32,
             Conntrack::State => libc::NFT_CT_STATE as u32,
         }
     }
 }
 
-impl Expression for Conntrack {
+impl Expression for ConntrackStruct {
     fn to_expr(&self) -> *mut sys::nftnl_expr {
         unsafe {
             let expr = try_alloc!(sys::nftnl_expr_alloc(b"ct\0" as *const _ as *const c_char));
 
-            sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CT_DREG as u16, libc::NFT_REG_1 as u32);
+            if self.set_value {
+                sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CT_SREG as u16, libc::NFT_REG_1 as u32);
+            } else {
+                sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CT_DREG as u16, libc::NFT_REG_1 as u32);
+            }
             sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CT_KEY as u16, self.raw_key());
 
             expr
@@ -40,6 +55,21 @@ impl Expression for Conntrack {
 #[macro_export]
 macro_rules! nft_expr_ct {
     (state) => {
-        $crate::expr::Conntrack::State
+        $crate::expr::ConntrackStruct::new(
+            $crate::expr::Conntrack::State,
+            false,
+        )
+    };
+    (mark set) => {
+        $crate::expr::ConntrackStruct::new(
+            $crate::expr::Conntrack::Mark,
+            true,
+        )
+    };
+    (mark) => {
+        $crate::expr::ConntrackStruct::new(
+            $crate::expr::Conntrack::Mark,
+            false,
+        )
     };
 }
