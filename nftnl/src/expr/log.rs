8de34cdb082b837b47d0211cdf96d9dcf60067fc
Kyle Kaniecki <kanieckikyle@gmail.com>
2024-03-14 22:17:23 +0000

    no type?

--- nftnl/src/expr/log.rs
+++ nftnl/src/expr/log.rs
@@ -26,7 +26,7 @@ impl Expression for Log {
 
 #[macro_export]
 macro_rules! nft_expr_log {
-    (group $group:expr, u16) => {
+    (group $group:expr) => {
         $crate::expr::Log::new($group)
     };
     () => {
