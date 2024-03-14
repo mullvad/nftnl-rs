6d89fe772844e0ff1f46c49a618783f89aae09f8
Kyle Kaniecki <kanieckikyle@gmail.com>
2024-03-14 22:24:47 +0000

    literally dude

--- nftnl/src/expr/log.rs
+++ nftnl/src/expr/log.rs
@@ -26,7 +26,7 @@ impl Expression for Log {
 
 #[macro_export]
 macro_rules! nft_expr_log {
-    (group $group:expr) => {
+    (group $group:literal) => {
         $crate::expr::Log::new($group)
     };
     () => {
