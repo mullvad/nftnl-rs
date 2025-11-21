580d42d7c19faec0a84e708a2badeb8b0c2a042d
Markus Pettersson <markus.pettersson@mullvad.net>
2025-11-21 15:32:43 +0100

    Move nft_expr_socket macro to `expr::socket` module

--- nftnl/src/expr/socket.rs
+++ nftnl/src/expr/socket.rs
@@ -1,66 +1,94 @@
-use crate::expr::Register;
+//! Socket expressions.
+//!
+//! Socket expressions are available since nftnl 1.2.0.
 
-use super::{Expression, Rule};
-use nftnl_sys::{self as sys};
-use std::ptr;
+#[cfg(feature = "nftnl-1-2-0")]
+pub mod imp {
+    use crate::Rule;
+    use crate::expr::{Expression, Register};
 
-pub struct Socket {
-    key: SocketKey,
-    register: Register,
-    level: u32,
-}
+    use nftnl_sys::{self as sys};
+    use std::ptr;
 
-impl Socket {
-    pub fn new(key: SocketKey, register: Register, level: u32) -> Self {
-        Socket {
-            level,
-            register,
-            key,
+    pub struct Socket {
+        key: SocketKey,
+        register: Register,
+        level: u32,
+    }
+
+    impl Socket {
+        pub fn new(key: SocketKey, register: Register, level: u32) -> Self {
+            Socket {
+                level,
+                register,
+                key,
+            }
         }
     }
-}
 
-impl Expression for Socket {
-    fn to_expr(&self, _rule: &Rule) -> ptr::NonNull<sys::nftnl_expr> {
-        unsafe {
-            let expr = try_alloc!(sys::nftnl_expr_alloc(c"socket".as_ptr()));
-            // In the source code for socket expr, the member values are validated to be of type 'MNL_TYPE_U32'
-            // https://git.netfilter.org/libnftnl/tree/src/expr/socket.c
-            sys::nftnl_expr_set_u32(
-                expr.as_ptr(),
-                sys::NFTNL_EXPR_SOCKET_KEY as u16,
-                self.key.to_raw(),
-            );
-            sys::nftnl_expr_set_u32(
-                expr.as_ptr(),
-                sys::NFTNL_EXPR_SOCKET_DREG as u16,
-                self.register.to_raw(),
-            );
-            sys::nftnl_expr_set_u32(
-                expr.as_ptr(),
-                sys::NFTNL_EXPR_SOCKET_LEVEL as u16,
-                self.level,
-            );
-            expr
+    impl Expression for Socket {
+        fn to_expr(&self, _rule: &Rule) -> ptr::NonNull<sys::nftnl_expr> {
+            unsafe {
+                let expr = try_alloc!(sys::nftnl_expr_alloc(c"socket".as_ptr()));
+                // In the source code for socket expr, the member values are validated to be of type 'MNL_TYPE_U32'
+                // https://git.netfilter.org/libnftnl/tree/src/expr/socket.c
+                sys::nftnl_expr_set_u32(
+                    expr.as_ptr(),
+                    sys::NFTNL_EXPR_SOCKET_KEY as u16,
+                    self.key.to_raw(),
+                );
+                sys::nftnl_expr_set_u32(
+                    expr.as_ptr(),
+                    sys::NFTNL_EXPR_SOCKET_DREG as u16,
+                    self.register.to_raw(),
+                );
+                sys::nftnl_expr_set_u32(
+                    expr.as_ptr(),
+                    sys::NFTNL_EXPR_SOCKET_LEVEL as u16,
+                    self.level,
+                );
+                expr
+            }
         }
     }
+
+    #[repr(u32)]
+    #[derive(Clone, Copy, Debug)]
+    pub enum SocketKey {
+        /// NFT_SOCKET_TRANSPARENT
+        Transparent = 0,
+        /// NFT_SOCKET_MARK
+        Mark = 1,
+        /// NFT_SOCKET_WILDCARD
+        Wildcard = 2,
+        /// NFT_SOCKET_CGROUPV2
+        CgroupV2 = 3,
+    }
+
+    impl SocketKey {
+        pub fn to_raw(self) -> u32 {
+            self as u32
+        }
+    }
+
+    #[macro_export(local_inner_macros)]
+    macro_rules! nft_expr_socket {
+    (socket cgroupv2 level $level:expr) => {
+        nft_expr_socket!(socket (::nftnl::expr::SocketKey::CgroupV2) level $level)
+    };
+    (socket ($key:expr) level $level:expr) => {
+        // TODO: why do we need to specify register??
+        ::nftnl::expr::Socket::new($key, ::nftnl::expr::Register::Reg1, $level)
+    };
+}
 }
 
-#[repr(u32)]
-#[derive(Clone, Copy, Debug)]
-pub enum SocketKey {
-    /// NFT_SOCKET_TRANSPARENT
-    Transparent = 0,
-    /// NFT_SOCKET_MARK
-    Mark = 1,
-    /// NFT_SOCKET_WILDCARD
-    Wildcard = 2,
-    /// NFT_SOCKET_CGROUPV2
-    CgroupV2 = 3,
-}
-
-impl SocketKey {
-    pub fn to_raw(self) -> u32 {
-        self as u32
+#[cfg(not(feature = "nftnl-1-2-0"))]
+mod imp {
+    #[macro_export(local_inner_macros)]
+    macro_rules! nft_expr_socket {
+        ($($_:tt)+) => {
+            ::std::compile_error!("This feature requires feature 'nftnl-1-2-0'");
+        };
     }
 }
