4b986a2800f6595b48247a32cb0da96c3c9db9fd
Markus Pettersson <markus.pettersson@mullvad.net>
2025-11-21 15:27:34 +0100

    Cleanup `expr/socket.rs`

--- nftnl/src/expr/socket.rs
+++ nftnl/src/expr/socket.rs
@@ -6,28 +6,8 @@ use std::ptr;
 
 pub struct Socket {
     key: SocketKey,
-    /// https://git.netfilter.org/libnftnl/tree/include/linux/netfilter/nf_tables.h
-    register: Register, // TODO: Always immediate?
+    register: Register,
     level: u32,
-}
-
-#[repr(u32)] // See nftnl_expr_socket_parse
-#[derive(Clone, Copy, Debug)]
-pub enum SocketKey {
-    /// NFT_SOCKET_TRANSPARENT
-    Transparent = 0,
-    /// NFT_SOCKET_MARK
-    Mark = 1,
-    /// NFT_SOCKET_WILDCARD
-    Wildcard = 2,
-    /// NFT_SOCKET_CGROUPV2
-    CgroupV2 = 3,
-}
-
-impl SocketKey {
-    pub fn to_raw(self) -> u32 {
-        self as u32
-    }
 }
 
 impl Socket {
@@ -49,7 +29,7 @@ impl Expression for Socket {
             sys::nftnl_expr_set_u32(
                 expr.as_ptr(),
                 sys::NFTNL_EXPR_SOCKET_KEY as u16,
-                self.key as u32,
+                self.key.to_raw(),
             );
             sys::nftnl_expr_set_u32(
                 expr.as_ptr(),
@@ -63,5 +43,24 @@ impl Expression for Socket {
             );
             expr
         }
+    }
+}
+
+#[repr(u32)]
+#[derive(Clone, Copy, Debug)]
+pub enum SocketKey {
+    /// NFT_SOCKET_TRANSPARENT
+    Transparent = 0,
+    /// NFT_SOCKET_MARK
+    Mark = 1,
+    /// NFT_SOCKET_WILDCARD
+    Wildcard = 2,
+    /// NFT_SOCKET_CGROUPV2
+    CgroupV2 = 3,
+}
+
+impl SocketKey {
+    pub fn to_raw(self) -> u32 {
+        self as u32
     }
 }
