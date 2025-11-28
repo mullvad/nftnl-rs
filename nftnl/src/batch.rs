mullvad/nftnl-rs

commit 4e54e253521141f0bfeef7d83d6e83932ab2cc56

Set F_ACK on all netlink messages
> `NLMSG_ERROR` and `NLMSG_DONE` are of practical importance. They carry
> return codes for operations. Note that unless the NLM_F_ACK flag is
> set on the request Netlink will not respond with NLMSG_ERROR if there
> is no error. To avoid having to special-case this quirk it is
> recommended to always set NLM_F_ACK.

source:
https://www.kernel.org/doc/html/next/userspace-api/netlink/intro.html

nftnl/src/batch.rs
@@ -1,5 +1,6 @@
 use crate::{MsgType, NlMsg};
 use core::fmt;
+use nftnl_sys::libc::NLM_F_ACK;
 use nftnl_sys::{self as sys, libc};
 use std::ffi::c_void;
 use std::os::raw::c_char;
@@ -111,12 +112,18 @@ impl Batch {
     }
 
     fn write_begin_msg(&mut self) {
-        unsafe { sys::nftnl_batch_begin(self.current().cast::<c_char>(), self.seq) };
+        let buf_ptr = self.current().cast::<c_char>();
+        let nlmsghdr = unsafe { sys::nftnl_batch_begin(buf_ptr, self.seq) };
+        let mut nlmsghdr = ptr::NonNull::new(nlmsghdr).expect("nlmsg_build_hdr never returns null");
+        unsafe { nlmsghdr.as_mut() }.nlmsg_flags |= NLM_F_ACK as u16; // all messages should set F_ACK
         self.next();
     }
 
     fn write_end_msg(&mut self) {
-        unsafe { sys::nftnl_batch_end(self.current().cast::<c_char>(), self.seq) };
+        let buf_ptr = self.current().cast::<c_char>();
+        let nlmsghdr = unsafe { sys::nftnl_batch_end(buf_ptr, self.seq) };
+        let mut nlmsghdr = ptr::NonNull::new(nlmsghdr).expect("nlmsg_build_hdr never returns null");
+        unsafe { nlmsghdr.as_mut() }.nlmsg_flags |= NLM_F_ACK as u16; // all messages should set F_ACK
         self.next();
     }
 

nftnl/src/rule.rs
@@ -77,8 +77,11 @@ unsafe impl crate::NlMsg for Rule<'_> {
             MsgType::Del => libc::NFT_MSG_DELRULE,
         };
         let flags: u16 = match msg_type {
-            MsgType::Add => (libc::NLM_F_CREATE | libc::NLM_F_APPEND | libc::NLM_F_EXCL) as u16,
-            MsgType::Del => 0u16,
+            MsgType::Add => {
+                (libc::NLM_F_CREATE | libc::NLM_F_APPEND | libc::NLM_F_EXCL | libc::NLM_F_ACK)
+                    as u16
+            }
+            MsgType::Del => libc::NLM_F_ACK as u16,
         };
         let header = unsafe {
             sys::nftnl_nlmsg_build_hdr(

nftnl/src/table.rs
@@ -81,7 +81,7 @@ pub fn get_tables_nlmsg(seq: u32) -> Vec<u8> {
             buffer.as_mut_ptr().cast::<c_char>(),
             libc::NFT_MSG_GETTABLE as u16,
             ProtoFamily::Unspec as u16,
-            (libc::NLM_F_ROOT | libc::NLM_F_MATCH) as u16,
+            (libc::NLM_F_ROOT | libc::NLM_F_MATCH | libc::NLM_F_ACK) as u16,
             seq,
         )
     };

commit db148f257e101d9672e7e9fc707f663a171fa608

Remove NLM_F_ACK from batch begin/end messages
Kernels prior to 6.10 do not reply with an ACK for batch messages, so
NLM_F_ACK is removed from batch messages uniformly to avoid waiting for
ACKs that will never arrive.

Fixes: 4e54e2535211 ("Set F_ACK on all netlink messages")

nftnl/src/batch.rs
@@ -1,6 +1,5 @@
 use crate::{MsgType, NlMsg};
 use core::fmt;
-use nftnl_sys::libc::NLM_F_ACK;
 use nftnl_sys::{self as sys, libc};
 use std::ffi::c_void;
 use std::ops::Range;
@@ -115,18 +114,12 @@ impl Batch {
     }
 
     fn write_begin_msg(&mut self) {
-        let buf_ptr = self.current().cast::<c_char>();
-        let nlmsghdr = unsafe { sys::nftnl_batch_begin(buf_ptr, self.seqs.end) };
-        let mut nlmsghdr = ptr::NonNull::new(nlmsghdr).expect("nlmsg_build_hdr never returns null");
-        unsafe { nlmsghdr.as_mut() }.nlmsg_flags |= NLM_F_ACK as u16; // all messages should set F_ACK
+        unsafe { sys::nftnl_batch_begin(self.current().cast::<c_char>(), self.seqs.end) };
         self.next();
     }
 
     fn write_end_msg(&mut self) {
-        let buf_ptr = self.current().cast::<c_char>();
-        let nlmsghdr = unsafe { sys::nftnl_batch_end(buf_ptr, self.seqs.end) };
-        let mut nlmsghdr = ptr::NonNull::new(nlmsghdr).expect("nlmsg_build_hdr never returns null");
-        unsafe { nlmsghdr.as_mut() }.nlmsg_flags |= NLM_F_ACK as u16; // all messages should set F_ACK
+        unsafe { sys::nftnl_batch_end(self.current().cast::<c_char>(), self.seqs.end) };
         self.next();
     }
 
@@ -174,9 +167,10 @@ impl FinalizedBatch {
         }
     }
 
-    /// The range of sequence numbers assigned to the messages in this batch.
+    /// Returns the range of sequence numbers for the messages in this batch that expect an ACK.
     pub fn sequence_numbers(&self) -> Range<u32> {
-        self.batch.seqs.clone()
+        // The first (BATCH_BEGIN) and last (BATCH_END) messages do not expect an ACK.
+        (self.batch.seqs.start + 1)..(self.batch.seqs.end - 1)
     }
 }
 